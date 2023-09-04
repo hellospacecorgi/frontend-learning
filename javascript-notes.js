f5536e64a562e102210077dfa92c2a116630e367
Flora Chan <chanhiuwun@hotmail.com>
2023-09-04 11:19:01 +1000

    Added notes for asyn await promises

--- javascript-notes.js
+++ javascript-notes.js
@@ -6,7 +6,7 @@ const c = 3;
 let myName;
 console.log(myName); // undefined
 myName = 'John';
-console.log(myName); // Flora
+console.log(myName); // John
 
 /* Primitive/Value Data Types
     * 1. String
@@ -45,6 +45,23 @@ selectedColors[2] = 'green';
 selectedColors[3] = 1;
 console.log(selectedColors.length);
 
+let names = ['John', 'Mary', 'Bob', 'Bob', 'Katie'];
+for (let i = 0; i < names.length; i++) {
+  names[i] = names[i] + ' Smith';
+}
+
+names.forEach((name) => {
+  name = name + ' Smith';
+})
+
+names.map((name) => {
+  name = name + ' Smith';
+})
+
+names.filter((name) => {
+  return name != 'Bob';
+})
+
 //Functions
 function greet(name, lastName) {
   console.log('Hello ' + name + ' ' + lastName);
@@ -56,3 +73,75 @@ function square(number) {
     return number * number;
 }
 console.log(square(2));
+
+// Arrow functions
+const MyComponent = () => {
+  return <div><h1>hello</h1></div>
+}
+/**
+ * export default function
+ * export const
+ */
+
+//Anonymous function
+<button onClick={() => {console.log('Clicked');}}>Click me</button>
+
+// turnary operator
+let x = 10;
+const color = x > 10 ? 'red' : 'blue';
+let colorText = x > 10 && 'red';
+
+
+// Async + Await + Fetch
+const event = new Promise((resolve, reject) => {
+  var name = 'John';
+  if (name == 'John') {
+    resolve(); // function on success
+  } else {
+    reject('Name is not John'); // function on failure
+  }
+});
+
+event.then((name) => {
+  console.log(name); // data received 
+}).catch((err) => {
+  console.log(err);
+}).finally(() => {
+  console.log('always runs');
+});
+
+const axios = require("axios"); //allows us to fetch apis
+const promise = axios.get("https://cat-fact.herokuapp.com/facts");
+
+data.then((response) => {
+  console.log(response.data);
+}).catch((err) => {
+  console.log(err);
+});
+
+
+// async + await
+async function fetchData() {
+  try {
+    const promise = await axios.get("https://cat-fact.herokuapp.com/facts");
+    console.log(promise.data);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    console.log('always runs');
+  }
+  
+}
+
+const fetchData = async () => {
+  try {
+    const promise = await axios.get("https://cat-fact.herokuapp.com/facts");
+    console.log(promise.data);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    console.log('always runs');
+  }
+}
+
+fetchData
